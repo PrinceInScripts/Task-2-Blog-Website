a2aa6490c50131dd47f8225d349122154e70045e
princekumar <princepk7878kumar@gmail.com>
2024-04-08 00:30:55 +0530

    done user

--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -1,8 +1,18 @@
-import { Router } from "express";
-// import { register } from "../controller/user.controller.js";
+import { Router } from "express"
+import { isUserLoggedIn } from "../middlewares/auth.middelwares.js"
+import { addBlogToReadList, changePassword, getUser, logoutUser, readListPage, updateUserAvatar, updateUserCoverImage, updaterUserDetials } from "../controller/user.controllers.js"
+import { upload } from "../middlewares/multer.middelwares.js"
 
 const router=Router()
 
-// router.route("/register").post(register)
+router.route("/me").get(isUserLoggedIn,getUser)
+router.route("/logout").get(isUserLoggedIn,logoutUser)
+router.route("/change-password").post(isUserLoggedIn,changePassword)
+router.route("/update-account").patch(isUserLoggedIn,updaterUserDetials)
+router.route("/avatar").patch(isUserLoggedIn,upload.single("avatar"),updateUserAvatar)
+router.route("/coverImage").patch(isUserLoggedIn,upload.single("coverImage"),updateUserCoverImage)
+router.route("/read-list/:slug").post(isUserLoggedIn,addBlogToReadList)
+router.route("/read-list").get(isUserLoggedIn,readListPage)
+
 
 export default router;
