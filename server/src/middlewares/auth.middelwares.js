159c4f7f8d7bbb6b7460c04a48e1917bcd584c73
princekumar <princepk7878kumar@gmail.com>
2024-04-07 23:58:06 +0530

    done auth middleware

---
+++ server/src/middlewares/auth.middelwares.js
@@ -0,0 +1,29 @@
+import { User } from "../models/user.models.js";
+import { ApiError } from "../utils/ApiError.js";
+import { asyncHandler } from "../utils/AsyncHander.js";
+import jwt from 'jsonwebtoken'
+
+
+
+export const isUserLoggedIn=asyncHandler (async (req,res,next)=>{
+    try {
+        const token=req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ","")
+
+        if(!token){
+            throw new ApiError(401,"Unathorized request")
+        }
+
+        const decodedToken=jwt.verify(token,process.env.ACCESS_TOKEN_SECRET)
+
+        const user=await User.findById(decodedToken._id).select("-password -refreshToken")
+
+        if(!user){
+            throw new ApiError(401,"Invalid Access token")
+        }
+
+        req.user=user
+        next()
+    } catch (error) {
+        throw new ApiError(401,error?.message || "Invalid acxess token")
+    }
+})
